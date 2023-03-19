719a41582fae6caf82473358811c02b006a679e2
andrew <57403896+andusha@users.noreply.github.com>
2023-03-20 00:37:05 +0700

    complete display block

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 const buttons = document.querySelectorAll(".button");
+const topDisplay = document.querySelector(".top-display")
+const bottomDisplay = document.querySelector('.bottom-display')
 
 let digits = [];
 let operators = [];
 let digitString = "";
 let displayTopString = ''
-let displayBottomSring = ''
 
 buttons.forEach((elem) => {
   elem.addEventListener("click", () => {
@@ -13,28 +14,39 @@ buttons.forEach((elem) => {
     if (elemDataClass === "digit") {
       console.log(elem.innerHTML);
       digitString += elem.innerHTML;
+      bottomDisplay.innerHTML += elem.innerHTML;
     } else if (elemDataClass === "operator") {
       if (digitString) {
         digits.push(digitString);
         digitString = "";
       }
       operators.push(elem.innerHTML);
-      if (operators.length > 1 && digits.length <= 1)
-        operators = [elem.innerHTML];
+
+      if (operators.length > 1 && digits.length <= 1) operators = [elem.innerHTML];
       if (operators.length == 1 && operators[0] == "=") {
         operators = [];
         digits = [digits[0]];
+      } else {
+        topDisplay.innerHTML = digits[0] + operators[0]
+        bottomDisplay.innerHTML = ''
       }
-      console.log(digits.length, digits, operators.length);
+
+
       if (digits.length >= 2 && operators.length >= 1) {
         let calc = calculate(
           Number(digits[0]),
           operators[0],
           Number(digits[1])
         );
+        topDisplay.innerHTML = digits[0] + operators[0] + digits[1] + ' ='
+        bottomDisplay.innerHTML = calc
         digits = [calc];
         if (elem.innerHTML == "=") operators = [];
-        else operators = [elem.innerHTML];
+        else {
+            operators = [elem.innerHTML];
+            topDisplay.innerHTML = digits[0] + operators[0]
+            bottomDisplay.innerHTML = ''
+        }
       }
     }
   });
