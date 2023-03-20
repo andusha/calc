8040d2c03627c44059be1ccc2a61d7f7462ef256
andrew <57403896+andusha@users.noreply.github.com>
2023-03-20 14:09:01 +0700

    complete calculator

---
+++ gg.js
@@ -0,0 +1,4 @@
+"use strict";
+let b = '-150.25'
+let c = Number(b)
+console.log(c + 10)
--- script.js
+++ script.js
@@ -2,27 +2,31 @@
 const buttons = document.querySelectorAll(".button");
 const topDisplay = document.querySelector(".top-display")
 const bottomDisplay = document.querySelector('.bottom-display')
+const decimalButton = document.querySelector('.decimal')
 
 let digits = [];
 let operators = [];
 let digitString = "";
-let displayTopString = ''
 
 buttons.forEach((elem) => {
   elem.addEventListener("click", () => {
     let elemDataClass = elem.getAttribute("data-class");
     if (elemDataClass === "digit") {
-      console.log(elem.innerHTML);
+      if (digitString.includes('.')) decimalButton.style.pointerEvents='none';
+      else decimalButton.style.pointerEvents='auto';
       digitString += elem.innerHTML;
-      bottomDisplay.innerHTML += elem.innerHTML;
+      bottomDisplay.innerHTML = digitString;
+
     } else if (elemDataClass === "operator") {
       if (digitString) {
+        console.log(digitString)
         digits.push(digitString);
         digitString = "";
       }
       operators.push(elem.innerHTML);
 
       if (operators.length > 1 && digits.length <= 1) operators = [elem.innerHTML];
+      if (operators.length >= 1 && digits.length == 0) digits.push(0);
       if (operators.length == 1 && operators[0] == "=") {
         operators = [];
         digits = [digits[0]];
@@ -31,27 +35,58 @@ buttons.forEach((elem) => {
         bottomDisplay.innerHTML = ''
       }
 
-
       if (digits.length >= 2 && operators.length >= 1) {
         let calc = calculate(
           Number(digits[0]),
           operators[0],
           Number(digits[1])
         );
-        topDisplay.innerHTML = digits[0] + operators[0] + digits[1] + ' ='
-        bottomDisplay.innerHTML = calc
-        digits = [calc];
-        if (elem.innerHTML == "=") operators = [];
-        else {
-            operators = [elem.innerHTML];
-            topDisplay.innerHTML = digits[0] + operators[0]
-            bottomDisplay.innerHTML = ''
+          if (calc == Infinity) {
+            topDisplay.innerHTML = ''
+            bottomDisplay.innerHTML = 'Error'
+            digits = []
+          } else{
+          topDisplay.innerHTML = digits[0] + operators[0] + digits[1] + ' ='
+          bottomDisplay.innerHTML = calc
+          digits = []
+          digitString = String(calc)
+          if (elem.innerHTML == "=") operators = [];
+          else {
+              digits = [calc]
+              operators = [elem.innerHTML];
+              digitString = ''
+              topDisplay.innerHTML = calc + operators[0]
+              bottomDisplay.innerHTML = ''
+          }
         }
       }
-    }
+    } else {
+       if (elem.innerHTML == 'A') {
+        digitString = digitString.slice(0, -1)
+        bottomDisplay.innerHTML = digitString
+      } else if (elem.innerHTML == 'AC'){
+        digitString = ''
+        digits = []
+        operators = []
+        bottomDisplay.innerHTML = ''
+        topDisplay.innerHTML = ''
+      } else {
+          if (digits.length == 1 && operators.length == 0){
+            console.log(digits[0])
+            if (Math.sign(digits[0]) == 1) digits[0] = '-' + digits[0]
+            else digits[0] = digits[0].slice(1)
+            bottomDisplay.innerHTML = digits[0]
+          } else {
+              if (Math.sign(digitString) == 1) digitString = '-' + digitString
+              else digitString = digitString.slice(1)
+              bottomDisplay.innerHTML = digitString
+          }
+      }
+    } 
   });
 });
 function calculate(firstDigit, operator, secondDigit) {
+  console.log(firstDigit, operator, secondDigit)
   if (operator == "+") return firstDigit + secondDigit;
   else if (operator == "-") return firstDigit - secondDigit;
   else if (operator == "%") return firstDigit % secondDigit;
